66c68b756212d3c2a8744523d655b818838288ef
Mikoláš Belec <miko.belec@gmail.com>
2019-01-20 14:36:08 +0100

    added ignored files

---
+++ eObcanka.NET/Enums/AppId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eObcanka.NET.Enums
+{
+   public class AppId
+    {
+
+        internal static byte[] APP_ID_CARD_MANAGEMENT = { 0xD2, 0X03, 0x10, 0x01, 0x00, 0x01, 0x00, 0x02, 0x02 };
+        internal static byte[] APP_ID_FILE_MANAGEMENT = { 0xD2, 0x03, 0x10, 0x01, 0x00, 0x01, 0x03, 0x02, 0x01, 0x00 };
+    }
+}
